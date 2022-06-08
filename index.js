a139377ed318fc17251569c8e56e3e47ae40f57b
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-06-09 02:08:53 +0600

    Built-in Middleware

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express();
 const fs = require("fs");
 const studentRouter=require('./routers/studentRouter');
 
-
-
-
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+
 app.use((req,res,next)=>{
   console.log("I am Middleware 1");
   next();
