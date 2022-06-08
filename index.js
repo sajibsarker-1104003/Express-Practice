08caf2e30645b8197d1dd296de491d6616c31b3b
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-06-08 13:49:56 +0600

    Fs module=>read

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
  const express=require('express');
  const app=express();
 
+ const fs=require('fs');
+
  app.get('/', (req,res)=>{
    res.send('hello world from Express js!!');
 
  })
 
- const port=3003;
+ app.get('/api/students',(req,res)=>{
+   fs.readFile('./db.json','utf-8',(error,data)=>{
+     console.log(data);
+
+     const student=JSON.parse(data).students;
+     res.send(student);
+
+   })
+
+ })
+
+ const port=3000;
 
  app.listen(port, ()=>{
-   console.log(`Listening on port 3003`);
+   console.log(`Listening on port 3000`);
  })
